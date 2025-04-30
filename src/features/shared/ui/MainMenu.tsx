0a3e0aeeae0d6e6071c5338fe080572b93b5b562
Flavio Silva <flsilva7@gmail.com>
2025-04-30 10:49:59 -0300

    complete refactoring to Next.js v15 App Router

---
+++ src/features/shared/ui/MainMenu.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+import { textClassName } from "./typography/textClassName";
+import { A } from "./typography/Typography";
+
+interface MainMenuProps {
+  readonly onItemClick?: () => void;
+}
+
+export const MainMenu = ({ onItemClick }: MainMenuProps) => {
+  const menuItemClassName =
+    "flex justify-center w-full border-b border-primary-text mt-1 pt-4 pb-4 lg:border-b-0 lg:justify-normal lg:ml-8 lg:p-0 lg:w-auto";
+  const menuItemLinkClassName = "font-bold hover:underline";
+  const dynamicProps = onItemClick ? { onClick: onItemClick } : {};
+
+  return (
+    <nav className="w-full overflow-hidden mt-8 lg:mt-0">
+      <ul className="flex flex-col items-center m-0 p-0 lg:flex-row lg:items-start lg:m-[-0.375rem_0_0_4rem]">
+        <li className={menuItemClassName}>
+          <Link
+            href="/docs/asdoc"
+            className={twMerge(textClassName, menuItemLinkClassName)}
+            {...dynamicProps}
+          >
+            ASDOC
+          </Link>
+        </li>
+        <li className={menuItemClassName}>
+          <A
+            href="https://github.com/flsilva/as3utils"
+            className={twMerge(textClassName, menuItemLinkClassName)}
+            target="_blank"
+          >
+            Source
+          </A>
+        </li>
+        <li className={menuItemClassName}>
+          <A
+            href="/download/as3utils-1.0-distribution.zip"
+            className={twMerge(textClassName, menuItemLinkClassName)}
+            target="_blank"
+          >
+            Download
+          </A>
+        </li>
+      </ul>
+    </nav>
+  );
+};
---
+++ src/features/shared/ui/logo/Logo.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface LogoProps {
+  readonly onClick?: () => void;
+}
+
+export const Logo = ({ onClick }: LogoProps) => (
+  <h1 className="flex">
+    <Link href="/" onClick={onClick}>
+      <Image src="/images/logo@2x.png" alt="AS3Utils" width={133} height={56} />
+    </Link>
+  </h1>
+);
